ae281fe9bf5be49d0045e6d7c96e62e104a4bd71
billyk01 <amoako.billyking@gmail.com>
2020-09-23 12:32:59 +0200

    core(exo): Exercices fait

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/* Exo 1 */
+
+/* My Digital School Forever */
+
+/**
+* My Digital School forever every day
+**/
+
+/* Exo 3 */
+
+var string = '42';
+var number = 42
+var float = 42.0;
+var boolean = true; 
+var array = [42];
+var date = new Date[4241, 12, 24,42,42] ;
+var objet = {'42' : 42};
+var undefined;
+var empty=null;
+
+
+/* Exo 4 */
+
+var my42count = 'quarante-deux';
+var count = my42count.length;
+console.log(count);
+
