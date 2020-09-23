0dc41f3d5bbc3bb2a08304a0b1315ef85e49f336
billyk01 <amoako.billyking@gmail.com>
2020-09-23 16:16:19 +0200

    core(exo): Les conditions

--- index.js
+++ index.js
@@ -27,3 +27,28 @@ var my42count = 'quarante-deux';
 var count = my42count.length;
 console.log(count);
 
+
+
+/**
+* Les conditions
+*/
+
+
+/**
+*Exo 1
+*/
+
+
+var age = prompt('How old are you?');
+var clubEntry = (age >= 18 ) ? "Vous pouvez entrer vous êtes majeur vous avez " :
+ "Vous ne pouvez pas entrez vous n’êtes pas majeur vous avez";
+
+if(age > 42){
+alert("Patron");
+}else{
+alert(`${clubEntry}`+ ' '+ `${age} years old!`);
+}
+
+
+
+
